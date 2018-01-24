723c93d8a0da751560657caf31dd89108e18c304
Stephan <stephan.keller@student.tugraz.at>
2018-01-24 09:36:06 +0100

    update

--- competitive_new.py
+++ competitive_new.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Dec 19 11:31:41 2017
-
 @author: Stephan
 """
 
@@ -14,17 +12,15 @@ from math import pi, atan2
 # global vars
 max_x = 100 # xrange
 max_y = 100 # yrange
-number_of_costumers = 100
+number_of_customers = 50
 point_X = [1,1] # make sure it is set properly
-costumer_points = []
+customer_points = []
 
-################## TODO imported
-
-
+##################
+### Functions
 ##################
 def unit_vector(vector):
     """ Returns the unit vector of the vector.  """
-    # !TODO: Problem wenn ein Vektor = Nullvektor
     return vector / np.linalg.norm(vector)
 
 # returns angle in form of 0 to 2*pi
@@ -32,12 +28,16 @@ def angle_between(v1, v2):
     angle = atan2(v2[1]-v1[1], v2[0]-v1[0])
     if(angle<0):
         angle = 2*pi+angle
-# DEBUG
-#    print "angle between ", v1, " and ", v2, " is ", angle
     return angle
 
 
 def newline(p1, p2):
+    ax = plt.gca()
+    l = mlines.Line2D([p1[0], p2[0]], [p1[1], p2[1]])
+    ax.add_line(l)
+    return l
+
+def newline_infinite(p1, p2): # line until plot boundaries
     ax = plt.gca()
     xmin, xmax = ax.get_xbound()
 
@@ -54,11 +54,11 @@ def newline(p1, p2):
 ##################
 
 
-### TODO: change to sympy point datatype or create point struct
+###
 def create_random_points(number_of_points):
     points = []
     global max_x, max_y
-### TODO find out how to add point lavel/value
+### TODO add point label/value
     for x in range(0,number_of_points): #
         new_point = get_random_point(0, max_x, 0, max_y)
         points.append(new_point)
@@ -72,65 +72,54 @@ def get_random_point(min_x, max_x, min_y, max_y):
 def plot_points(points_to_plot):
     # set scale of plot to max_x, max_y
     plt.axis([0, max_x, 0, max_y])
-    plt.plot(*zip(*points_to_plot), marker='o', ls='', color='b') # damit die punkte wirklich als (x,y) koordinaten geplottet werden
+    plt.plot(*zip(*points_to_plot), marker='o', ls='', color='b') #plot as (x,y) coordinates
 
 ### plot a line through point_X in the radiant direction
 def plot_halfplane(point_X, vector):
-    #TODO calc norm vector
-    n_vector = [-vector[1],vector[0]]
-    tmp_point = [point_X[0]+n_vector[0], point_X[1]+n_vector[1]]
-#    print tmp_point
-    newline(point_X, tmp_point)
-# OLD
-#### plot a line through point_X in the radiant direction
-#def plot_halfplane(point_X, vector):
-#    tmp = [0,0]
-#    tmp[0] = point_X[0] + vector[0]
-#    tmp[1] = vector[1] + point_X[1]
-#    newline(point_X, tmp)
+    # calc norm vector for plotting halfplane
+    n_vector = get_norm_vector(vector)
+    u_vector = unit_vector(n_vector)
+    tmp_point = [point_X[0]+u_vector[0], point_X[1]+u_vector[1]]
+    newline_infinite(point_X, tmp_point)
+    print "vector: ", vector
+    plt.quiver(point_X[0], point_X[1], vector[0], vector[1], color=['r'], angles='xy', scale_units='xy', scale=1)
+    
+def get_norm_vector(vector):
+    return [-vector[1],vector[0]]
+
+def get_vector(p1,p2):
+    return [p2[0]-p1[0], p2[1]-p1[1]]
 
 ###Problem_1##############################################
-## TODO: find best location for Y
-# TODO: find the direction of each demand point from X and sort these directions in an
+## find best location for Y
+# find the direction of each demand point from X and sort these directions in an
 # increasing order between 0 and 2pi.
 def get_direction_from_X_to_point(point):
-# TODO: REMOVE dummy code     
-    # TODO: Replace with direction calc
-#    angle = random()*2*math.pi
-#    
-#    vector[0] = point[0]-point_X[0]
-#    vector[1] = point[1]-point_X[1]   
-#    angle = angle_clockwise(point_X, point) ########################### TODO: returns only 270
     global point_X
     angle = angle_between(point, point_X)
-    vector = [point[0]-point_X[0],point[1]-point_X[1]] # TODO ? normalvector
+    vector = [point[0]-point_X[0],point[1]-point_X[1]]
     
-    # !TODO check if correct
     #####################
     return angle, vector
-#print get_direction_from_X_to_point([0,0])
 
-
-# calculate all angles from point_X
+# calculate all angles (for sorting) & vectors from point_X
 def calculate_and_sort_all_point_directions():
-    global costumer_points    
+    global customer_points    
     dir_list = []
-    for point in costumer_points:
+    for point in customer_points:
         # return angle and corresponding point
         direction, vector = get_direction_from_X_to_point(point)
         dir_list.append([direction, vector])
     return sorted(dir_list)
     
 
-#def point_is_in_half_plane(line, point):
+# vector pointing into halfplane
 def point_is_in_half_plane(vector, point):
     def formula(x,y):
         d = vector[0]*point_X[0] + vector[1]*point_X[1]
         return (vector[0]*x + vector[1]*y -d)
-    # TODO: implement line check
     # accept on line
     tmp = formula(point[0],point[1])
-#    print tmp
     if (tmp>=0):
         return True
     else:
@@ -138,80 +127,178 @@ def point_is_in_half_plane(vector, point):
 
 def get_points_in_halfplane(vector):
     result = 0
-    global costumer_points
-    for point in costumer_points:
+    global customer_points
+    for point in customer_points:
         if point_is_in_half_plane(vector, point):
-            result += 1 # eventually add buying power to points
-    # TODO: calculate normal line to direction
-    #
-    # TODO: plot line
-    # TODO: check for all points if they're in the halfplane or not
-    return result # TODO ? return points already?
+            result += 1 # MOD add buying power to points
+    return result
 
 
     
 
-### PROBLEM 1 ###
-# Find optimal location for Y, given X and costumer points
+### PROBLEM 1
+# Find optimal location for Y, given X and customer points
 def find_optimal_location_for_Y():
-    global costumer_points
-    costumer_points = create_random_points(number_of_costumers) # create random distribution of buying power in a certain area
+    global customer_points
 #    plt.clf()# refresh the plot
-    
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    plot_points(costumer_points) 
+    plot_points(customer_points) 
     global point_X
-#    point_X = [randint(0,max_x),randint(0,max_y)]
     # a bit more central point X
     point_X = get_random_point(max_x/4, max_x*3/4, max_y/4, max_y*3/4)
-#    point_X = [max_x/2, max_y/2] # TODO Debug remove
-#    plt.plot(point_X[0], point_X[1] ,marker='o', color='r', label="X")
     ax.plot(point_X[0], point_X[1] ,marker='o', color='r', label="X")
     ax.annotate('X', xy=(point_X[0], point_X[1]))
     plt.show    
-    ### pre computation test
-#    print "TEST 0=", angle_between([-1,0],[1,0]) # 0    
-#    print "TEST pi/2=", angle_between([1,0],[1,1]) # pi/2
-#    print "TEST pi=", angle_between([1,0],[-1,0]) # pi
-#    print "TEST -pi/2=", angle_between([1,0],[1,-1]) # pi
-#    print "TEST -pi/4=", angle_between([0,0],[1,-1]) # pi
-#    print "TEST ~2pi=", angle_between([0,0],[1,-0.000001]) # pi
+    
     point_directions = calculate_and_sort_all_point_directions()
 #    print point_directions
     max_buying_power = 0
     best_half_plane_vector = [0,0]
     # TODO: Calculate in turn the buying power of all possible Y-half-planes
     for angle, vector in point_directions:
-#        print "angle ", angle
-#        print "vector ", vector
-#         TODO: get all points on half plane
         buying_power = get_points_in_halfplane(vector)
-#        print "Buying_power: ", buying_power, "in dir:", direction
+
         if (buying_power>max_buying_power):
             max_buying_power=buying_power
             best_half_plane_vector = vector
-#            print "new max bp: ", max_buying_power
-#            print "new best vector: ", vector
-#    plot_line(point_X)
+            # "animated" plot
+            plot_halfplane(point_X, best_half_plane_vector) 
+            print "Buying_power: ", buying_power
+            plt.pause(1.5) 
+
+
+#################################################################################################
+### PROBLEM 2
+def minimize_buying_power_of_Y():
+    print "solving problem 2"
+    global customer_points
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    plot_points(customer_points) 
+### Values needed for calculation
+    P_min = 0
+    P_max = 0
+    P_0 = 0
+    
+### Step 1. Calculate all lines through pairs of points and calculate all Pi for
+#each half-plane defined by the lines.    
+    bp_list = calculate_all_lines_and_buying_power()
+    if(not bp_list):
+        return 0
+    
+### Step 2. Sort Pi in decreasing order. Set Pmirl and P,,, to the lowest and
+#highest Pi I espectively.
+    sorted_bps = sort_buying_powers(bp_list)
+    P_min = sorted_bps[0][0] # lowest BP
+#    print ("Pmin", P_min)
+    P_max = sorted_bps[len(sorted_bps)-1][0] # highest BP
+#    print ("Pmax", P_max)
+    
+    ### TODO start loop until "last P_max"(?) here
+   ### if sorted_bps is empty go to step 7
+    while(sorted_bps):
+        sorted_bps = update_list_P_min_max(sorted_bps, P_min, P_max)
+### Step 3. Set Fe to the median value in the Pi vector for all rn,in < Pi < P,,,. If
+#there is IIO Pi fulfilling Pmin<Pi < I’,,,,, go to Step ‘7.
+    
+        P_0 = find_median_value(sorted_bps)
+        
+    ### Step 4. Find if there is a feasible point to all half-planes for which Vim P,.
+    #This can be done by linear programming.
+        if(find_feasible_point(sorted_bps, P_0)):  
+    ### Step 5. if there is a feasible solution point to the problem in Step 4 then
+    #minxiS( <P,. Set P,,, to P,, and go to Step 3.
+            P_max = P_0
+    ### Step 6. Otherwise minx(f(X)) 2 PO. Set Pmin to P,, and go to Step 3.
+        else:
+            P_min = P_0
+    
+### Step 7. A feasible point for the last P,,, is an optimal solution. The value of
+#the objective function is Ptnin.
+    ### TODO while schleife um step 3 bis 6
+    
+### remove all tuples where BP is <P_min and >P_max
+def update_list_P_min_max(sorted_bps, P_min, P_max):
+    updated_list = list()    
+    for bp_tuple in sorted_bps:
+        p_i = bp_tuple[0]
+        if (p_i> P_min and p_i<P_max):
+            updated_list.append(bp_tuple)
+    return updated_list
+
+            
+
+def calculate_all_lines_and_buying_power():
+    global customer_points
+    
+    # create list of all point pairs (non redundant - to minimize iterations)
+    point_pairs = []    
+    for point1 in range(len(customer_points)):
+        for point2 in range(point1+1, len(customer_points)):
+                point_pairs.append((customer_points[point1], customer_points[point2]))
+
+    buying_powers_list = list() # list of tuples (bp, p1, p2)
+    
+    for p1, p2 in point_pairs:
+### calculate buying power for both sides of the line and create two entries in the buying power list
+
+        newline(p1, p2)
+#        newline_infinite(point_pair)
+###PLOTTING#############
+#        newline(point_pair)
+########################        
+        # calculate normvector in both directions
+        norm_vector_left = get_norm_vector(get_vector(p1,p2))
+        norm_vector_right = get_norm_vector(get_vector(p2,p1))
+        # calc bp for both sides
+        bp_1 = get_points_in_halfplane(norm_vector_left)
+        bp_2 = get_points_in_halfplane(norm_vector_right)
+
+        buying_powers_list.append((bp_1,p1,p2)) 
+        buying_powers_list.append((bp_2,p2,p1))
+
+    return buying_powers_list
+    
+    
+
+def sort_buying_powers(bp_list):
+    ### sort in decreasing order
+    return sorted(bp_list)
+
+def find_median_value(sorted_bp_list):
+    
+    if(not sorted_bp_list):
+        return
+    # get median of the buying powers
+    median = np.median([(i[0]) for i in sorted_bp_list]) # only look at the buying power in the list of tuples
+    print "MEDIAN ", median
+    return median
+    
+def find_feasible_point(sorted_bps, P_0):
+    ### TODO find if there is a feasible point for all half planes, for which  P_i >= P_0
+
+### create half plane list for which P_i >= P_0
+    reduced_list = [i for i in sorted_bps if (i[0]>=P_0)]
+    print "find feasible point"
+    print "P0: ", P_0
+    print "Sorted_bps: ", sorted_bps
+    print "Reduced: ",reduced_list
+    if(True):
+        return 1
+    else:
+        return 0
+    
 ####################################
-## TODO just dummy, replace through
-#    best_half_plane_vector = [1,5] # TODO ! remove, testing only
-    #??????????????
-#    normal_dir = best_half_plane-pi/2 # TODO ? grenzfälle über 2pi und unter 0
-    #??????????????
-    
-#    if (normal_dir<0):
-#        normal_dir += 
-    plot_halfplane(point_X, best_half_plane_vector)    
-####################################    
-#TODO    plot_line(point_X, direction)
-    # Debugprints
-#    print "point direction angles", point_directions
-#    print "highest point direction ", np.amax(point_directions)
-#    print "max buying power: ", max_buying_power
-#    print "direction: ", direction
-#    print "------------"
-    
-### START ###
-find_optimal_location_for_Y()
+
+### Main ###
+def solve_problem(number):
+    global customer_points
+    customer_points = create_random_points(number_of_customers) # create random distribution of buying power in a certain area
+
+    if(number==1):
+        find_optimal_location_for_Y()
+    elif(number==2):
+        minimize_buying_power_of_Y()
+solve_problem(1)
+#solve_problem(2)
